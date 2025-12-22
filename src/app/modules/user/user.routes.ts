5e10b2e425d670ffe48265b2a5ff116979ebe6aa
mayesha <soumyrhmn234@gmail.com>
2025-12-23 01:47:38 +0600

    create user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -10,9 +10,9 @@ const router = Router()
 
 
 router.post("/register", UserControllers.createUser)
-router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
-router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe)
-router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getSingleUser)
-router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
+// router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
+// router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe)
+// router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getSingleUser)
+// router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
 
 export const UserRoutes = router
